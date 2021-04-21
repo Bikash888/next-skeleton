6f3beeaf13d5826a26ed9454b43c0972cc210e11
Bikash888 <bikashdulal150@gmail.com>
2021-04-21 15:42:17 +0545

    api added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import { QueryClient, QueryClientProvider } from 'react-query';
+import 'antd/dist/antd.css'; 
+// export function reportWebVitals(metric) {
+//   console.log("metric", metric)
+// }
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <QueryClientProvider client={queryClient}><Component {...pageProps} /></QueryClientProvider>
 }
 
 export default MyApp
--- pages/hotel/index.tsx
+++ pages/hotel/index.tsx
@@ -1,32 +1,55 @@
 import React from 'react';
 import Image from 'next/image';
 import Head from 'next/head';
+import axios from 'axios';
+import { useQuery } from 'react-query';
+import styles from './Hotel.module.css';
+import { Card, Typography } from 'antd'
 
+const getPostList = (): Promise<any> => {
+    return axios.get("http://private-anon-28db3fcbaa-bookstore.apiary-mock.com/data/books")
+
+}
+const { Meta } = Card;
+const { Paragraph } = Typography
 const Hotel = () => {
-    const image = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const { data: book } = useQuery("books", getPostList)
+    
     return (<>
         <Head>
             <title>Image Optimization In Next js</title>
             <meta name="description" content="Next Js Image optimization and performance" />
             <meta name="keywords" content="next/image,optimization,next js" />
         </Head>
+        <h1 className={styles.bookTop}>Book House</h1>
+        <div className={styles.cardGrid}>
+            {
+                book && book?.data.map((item, index) =>
+                    <Card key={index} hoverable className={styles.bookCard}
+                        cover={<Image quality={100} layout="intrinsic" src={item?.image} height={400} width={500} />}
+                    >  <div className={styles.bookInfo}>
+                            <strong>{item.title}</strong>
+                            <strong className={styles.author}>author:{item.author}</strong>
+                        </div>
+                        <Paragraph ellipsis={{ rows: 4, expandable: true, symbol: "read more" }}>{item.summary}</Paragraph>
+
+                    </Card>
+                )
+            }
+        </div>
         <div style={{
             display: "grid",
             gridTemplateColumns: "1fr 1fr 1fr",
             gap: "20px",
             margin: "20px"
         }}>
-
             {
-                Array.from(Array(20)).map((i, n) => <Image quality={100} key={n} alt="nature" src="/2.png" height={500} width={400} />)
+                Array.from(Array(20)).map((i, n) => <Image quality={100} key={n} alt="nature" src={`/sample.png`} height={500} width={400} />)
             }
-
-
-{
-            Array.from(Array(6)).map((i,n)=><Image quality={100} key={n} alt="nature" src={`/${n+1}.png`} height={500} width={400} />)
-        }
         </div>
-        
+
+
+
     </>
     )
 }
