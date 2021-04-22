a9d77435e0fe1117817850ec71a8bdb663435a46
Bikash888 <bikashdulal150@gmail.com>
2021-04-22 11:37:20 +0545

    minor updates

--- pages/hotel/index.tsx
+++ pages/hotel/index.tsx
@@ -4,21 +4,20 @@ import Head from 'next/head';
 import axios from 'axios';
 import { useQuery } from 'react-query';
 import styles from './Hotel.module.css';
-import { Card, Typography } from 'antd'
+import { Card, Typography,Spin } from 'antd'
 
 const getPostList = (): Promise<any> => {
     return axios.get("http://private-anon-28db3fcbaa-bookstore.apiary-mock.com/data/books")
 
 }
-const { Meta } = Card;
 const { Paragraph } = Typography
 const Hotel = () => {
-    const { data: book } = useQuery("books", getPostList)
+    const { data: book,isLoading } = useQuery("books", getPostList)
 
     return (<>
         <Head>
             <title>Image Optimization In Next js</title>
-
+            
             <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
             <meta name="description" content="Next Js Image optimization and performance" />
             <meta name="keywords" content="next/image,optimization,next js" />
@@ -26,7 +25,7 @@ const Hotel = () => {
         <h1 className={styles.bookTop}>Book House</h1>
         <div className={styles.cardGrid}>
             {
-                book && book?.data.map((item, index) =>
+                isLoading ? <Spin/> :book?.data.map((item, index) =>
                     <Card key={index} hoverable className={styles.bookCard}
                         cover={<Image  quality={100} alt="books-roamantic" src={item?.image} height={400} width={500} />}
                     >  <div className={styles.bookInfo}>
