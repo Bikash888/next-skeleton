f25438a8b4bc191866604537911f7c5a0f2a3d4f
Bikash888 <bikashdulal150@gmail.com>
2021-04-23 11:33:39 +0545

    ssr implemented

--- pages/hotel/index.tsx
+++ pages/hotel/index.tsx
@@ -4,16 +4,21 @@ import Head from 'next/head';
 import axios from 'axios';
 import { useQuery } from 'react-query';
 import styles from './Hotel.module.css';
-import { Card, Typography,Spin } from 'antd'
+import { Card, Typography,Spin } from 'antd';
+import {GetStaticProps} from 'next'
 
 const getPostList = (): Promise<any> => {
     return axios.get("http://private-anon-28db3fcbaa-bookstore.apiary-mock.com/data/books")
 
 }
 const { Paragraph } = Typography
-const Hotel = () => {
-    const { data: book,isLoading } = useQuery("books", getPostList)
 
+
+const Hotel =  (props) => {
+    
+     const { data: book,isLoading } = useQuery("books", getPostList,{initialData:props.finalData});
+   
+ 
     return (<>
         <Head>
             <title>Image Optimization In Next js</title>
@@ -25,7 +30,7 @@ const Hotel = () => {
         <h1 className={styles.bookTop}>Book House</h1>
         <div className={styles.cardGrid}>
             {
-                isLoading ? <Spin/> :book?.data.map((item, index) =>
+                isLoading ? <Spin/> :book?.data?.map((item, index) =>
                     <Card key={index} hoverable className={styles.bookCard}
                         cover={<Image  quality={100} alt="books-roamantic" src={item?.image} height={400} width={500} />}
                     >  <div className={styles.bookInfo}>
@@ -48,5 +53,10 @@ const Hotel = () => {
 
     </>
     )
+}
+export  const  getStaticProps:GetStaticProps=async()=>{
+    const posts:any = await getPostList();
+    const finalData= await posts?.data
+   return { props: { finalData} }
 }
 export default Hotel;
